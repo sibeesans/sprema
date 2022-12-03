a9acee4f9f529d0eb6ce103c12b221abbb20f782
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-03 12:42:09 +0700

    Update m-vmess.sh

--- menu/m-vmess.sh
+++ menu/m-vmess.sh
@@ -4,7 +4,7 @@ echo "Checking VPS"
 
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m       • VMESS ANNGUN MENU •         \E[0m"
+echo -e "\E[0;100;33m       • VMESS MENU •         \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create Account Vmess "
