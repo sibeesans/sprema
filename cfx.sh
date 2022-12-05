a06e939a692b1fef967ecc679948870a6088ef45
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-05 14:32:16 +0700

    Create cfx.sh

---
+++ cfx.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+apt install jq curl -y
+DOMAIN=sibe-vpn.live
+#sub=$(tr </dev/urandom -dc a-z0-9 | head -c4)
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
+SUB_DOMAIN=${sub}.sibe-vpn.live
+CF_ID=anfuji184@gmail.com
+CF_KEY=8700ccfd51dec7e1c3c95dd44250c7891b913
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com)
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+          -H "X-Auth-Email: ${CF_ID}" \
+          -H "X-Auth-Key: ${CF_KEY}" \
+          -H "Content-Type: application/json" \
+          --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN >/root/domain
+rm -f /root/cf.sh
